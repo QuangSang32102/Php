563b4924d645b9ac2590ab75b3c955d08135b6a5
Sang <quangsang0000@gmail.com>
2019-03-28 22:58:19 +0700

    v

--- 8.Array/Array/2/1.php
+++ 8.Array/Array/2/1.php
@@ -1,6 +1,6 @@
 <?php 
 	//Cach 1
-	$course = array();
+	/*$course = array();
 	$course[] = "PHP";		       //0
 	$course[] = "Zen FrameWork";   //1
 	$course[] = "Joomla";		   //2
@@ -10,10 +10,10 @@
 	$course[0] = "PHP";
 	$course[1] = "Zen FrameWork";
 	$course[2] = "Joomla";
-
+*/
 	//Cach 3
 	$course = array("PHP", "Zen FrameWork", "Joomla");
-	echo $course[0];
+	/*echo $course[0];*/
 
 	for ($i=0; $i < count($course); $i++) { 
 		echo $course[$i] . "<br>";
